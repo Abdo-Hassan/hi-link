c3566cc20476ce1286fd0f97fef4d0b2d840327f
abdohassan <ahassan@synapse-analytics.io>
2023-10-14 18:28:29 +0300

    styled get app section

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -5,11 +5,14 @@ type ButtonProps = {
   title: string;
   icon?: string;
   variant: string;
+  full?: boolean;
 };
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
-      className={`flexCenter gap-3 rounded-full  ${variant} cursor-pointer`}
+      className={`border flexCenter gap-3 rounded-full  ${variant} cursor-pointer ${
+        full && 'w-full'
+      }`}
       type={type}>
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
       <label className='bold-16 whitespace-nowrap cursor-pointer'>
