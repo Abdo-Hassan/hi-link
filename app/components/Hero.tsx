8ea49d624f57fd8062c5b25fcfce699816d45a65
abdohassan <ahassan@synapse-analytics.io>
2023-10-14 10:26:50 +0300

    enhance hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,25 +1,20 @@
 import Image from 'next/image';
-import Camp from '../../public/camp.svg';
-import Star from '../../public/star.svg';
-import Close from '../../public/close.svg';
-import Play from '../../public/play.svg';
 import Button from './Button';
 
 const Hero = () => {
   return (
-    <section className='max-container padding-container flex-col gap-20  py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
+    <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
       <div className='hero-map' />
 
-      {/* Left side */}
-      <div className='relative z-20 flex flex-1 flex-col xl:w-1/2 pb-16'>
+      {/* left side */}
+      <div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
         <Image
-          src={Camp}
-          alt='Camp'
+          src='/camp.svg'
+          alt='camp'
           width={50}
           height={50}
           className='absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]'
         />
-
         <h1 className='bold-52 lg:bold-88'>Putuk Truno Camp Area</h1>
         <p className='regular-16 mt-6 text-gray-30 xl:max-w-[520px]'>
           We want to be on each of your journeys seeking the satisfaction of
@@ -34,8 +29,8 @@ const Hero = () => {
               .fill(1)
               .map((_, index) => (
                 <Image
+                  src='/star.svg'
                   key={index}
-                  src={Star}
                   alt='star'
                   width={24}
                   height={24}
@@ -44,44 +39,43 @@ const Hero = () => {
           </div>
 
           <p className='bold-16 lg:bold-20 text-blue-70'>
-            198K
+            198k
             <span className='regular-16 lg:regular-20 ml-1'>
               Excellent Reviews
             </span>
           </p>
         </div>
 
-        {/* action buttons */}
         <div className='flex flex-col w-full gap-3 sm:flex-row'>
           <Button type='button' title='Download App' variant='btn_green' />
           <Button
             type='button'
             title='How we work?'
-            variant='btn_white_green'
-            icon={Play}
+            icon='/play.svg'
+            variant='btn_white_text'
           />
         </div>
       </div>
 
-      <div className='relative flex flex-1 items-start '>
-        <div className='relative z-20 w-[268px] flex flex-col gap-8 rounded-3xl bg-green-90 px-7 py-7'>
+      {/* right side */}
+      <div className='relative flex flex-1 items-start'>
+        <div className='relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8'>
           <div className='flex flex-col'>
             <div className='flexBetween'>
               <p className='regular-16 text-gray-20'>Location</p>
-              <Image src={Close} alt='close' width={24} height={24} />
+              <Image src='/close.svg' alt='close' width={24} height={24} />
             </div>
-
             <p className='bold-20 text-white'>Aguas Calientes</p>
           </div>
 
           <div className='flexBetween'>
             <div className='flex flex-col'>
               <p className='regular-16 block text-gray-20'>Distance</p>
-              <p className='bold-20 text-white'> 173.28 mi</p>
+              <p className='bold-20 text-white'>173.28 mi</p>
             </div>
             <div className='flex flex-col'>
               <p className='regular-16 block text-gray-20'>Elevation</p>
-              <p className='bold-20 text-white'>2.040 Km</p>
+              <p className='bold-20 text-white'>2.040 km</p>
             </div>
           </div>
         </div>
