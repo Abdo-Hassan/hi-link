ca77d6ce54fe0974cb2869b8832f34c5183f473e
abdohassan <ahassan@synapse-analytics.io>
2023-10-14 11:27:56 +0300

    change name of the app & creating camp component

--- app/components/Camp.tsx
+++ app/components/Camp.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+interface CampProps {
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
+}
+
+const CampSite = ({
+  backgroundImage,
+  title,
+  subtitle,
+  peopleJoined,
+}: CampProps) => {
+  return (
+    <div
+      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat`}></div>
+  );
+};
 
 const Camp = () => {
-  return <div>Camp</div>;
+  return (
+    <section className='border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-2;0'>
+      <div className='hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]'>
+        <CampSite
+          backgroundImage='bg-bg-img-1'
+          title='Putuk Truno Camp'
+          subtitle='Prigen Pasuran'
+          peopleJoined='50+ Joined'
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Camp;
